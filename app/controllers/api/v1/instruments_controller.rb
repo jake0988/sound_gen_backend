67e91e7e4556efba6e8959323bbcd69c162a2aa2
Jake <jake098877@gmail.com>
2021-03-05 23:56:31 -0500

    Added serializers, changed index json to serializer

--- app/controllers/api/v1/instruments_controller.rb
+++ app/controllers/api/v1/instruments_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::InstrumentsController < ApplicationController
   def index
     instruments = Instrument.all
-    render json: instruments
+    options = {
+      include: [:tuning]
+    }
+    render json: InstrumentSerializer.new(instruments, options)
   end
 
   def create
@@ -10,10 +13,13 @@ class Api::V1::InstrumentsController < ApplicationController
       render json: instrument, status: :accepted
     else
       render json: {errors: instrument.errors.full_messages}, status: :unprocessible_entity
+    end
   end
 
   private
 
   def instrument_params
     params.require(:instrument).permit(:name, :description, :sound, :image, :tuning_id, :frequency)
+  end
+
 end
