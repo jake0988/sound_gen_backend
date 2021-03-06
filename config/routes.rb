e9b187e5d6bf23e43629ba00227ed5e35e2adf10
Jake <jake098877@gmail.com>
2021-03-05 23:20:20 -0500

    Instrument controller create

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :instruments, only: [:index]
+      resources :instruments, only: [:index, :create]
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 just = Tuning.create(name: "Just")
 pythagorean = Tuning.create(name: "Pythagorean")
 equal = Tuning.create(name: "Equal")
+
+sawtooth = Instrument.create(name: "Sawtooth", tuning_id: 1)
+sine = Instrument.create(name: "Sine", tuning_id: 2)
