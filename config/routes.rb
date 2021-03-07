4508aaac3bd008247ab9bed51942eb5b7b9d28d3
Jake <jake098877@gmail.com>
2021-03-06 20:45:38 -0500

    renames image column to image_url

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :instruments, only: [:index, :create]
+      resources :tunings, only: [:index]
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_031721) do
+ActiveRecord::Schema.define(version: 2021_03_07_014343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2021_03_05_031721) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
-    t.string "image"
+    t.string "image_url"
     t.integer "tuning_id"
   end
 
